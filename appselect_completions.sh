a21752c24a56a48b1392c4136f42f348c71ee542
James Blades <jwkblades+git@gmail.com>
2020-03-25 17:25:28 -0600

    Fix some usability issues.

--- appselect_completions.sh
+++ appselect_completions.sh
@@ -3,27 +3,29 @@
 __appselect_apps()
 {
     local app="${1:-}"
-    for item in $(find .appcache -maxdepth 1 -name "${app}*" -a -type d); do
-        echo "${item}" | cut -s -d/ -f2-
+    for item in $(find "${HOME}/bin/.appcache" -maxdepth 1 -name "${app}*" -a -type d); do
+        echo "${item}" | sed -e "s#${HOME}/bin/.appcache/\?##"
     done
 }
 
 __appselect_versions()
 {
     local app="${1}"
-    if [[ ! -d ".appcache/${app}" ]]; then
+    if [[ ! -d "${HOME}/bin/.appcache/${app}" ]]; then
         return 0
     fi
 
     local arch="$(arch)"
-    ls -l .appcache/${app}/*.appimage | awk '{print $9}' | sed -e "s#.appcache/${app}/${app}-\(.*\).${arch}.appimage#\\1#"
+    ls -l ${HOME}/bin/.appcache/${app}/*.appimage | \
+        awk '{print $9}' | \
+        sed -e "s#${HOME}/bin/.appcache/${app}/${app}-\(.*\).${arch}.appimage#\\1#"
 }
 
 __appselect_completions()
 {
     local cur="${COMP_WORDS[COMP_CWORD]}"
     local prev="${COMP_WORDS[COMP_CWORD-1]}"
-    local cmds="$(find .appcache -maxdepth 1 -a -type d)"
+    local cmds="$(find "${HOME}/bin/.appcache" -maxdepth 1 -a -type d)"
     local likely=""
 
     local likely=""
