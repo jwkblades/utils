90320da974d89aaadbf677c5fbe5f9293e31436b
James Blades <jwkblades+git@gmail.com>
2020-10-16 14:25:09 -0600

    D6 probabilities for rolling, where you specify a percent chance of a passing roll.

---
+++ d6Chance.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+from math import factorial
+
+def choose(a, b):
+    return factorial(a) / (factorial(b) * factorial(a - b))
+
+def chance(dice, passes):
+    num = sum([choose(dice, x) for x in range(0, dice - passes + 1)])
+    return num / (2 ** dice)
+
+def chance2(dice, passes, prob = 1/2):
+    pct = 0;
+    for x in range(0, dice - passes + 1):
+        pct += ((prob) ** (dice - x)) * ((1 - prob) ** (x)) * choose(dice, x)
+    return pct
+
+
+print("", end="\t")
+for i in range(1, 10):
+    print(i, end="\t")
+print()
+
+for dice in range(1, 20):
+    print(dice, end="\t")
+    for passes in range(1, min(dice + 1, 10)):
+        print("{}".format(chance2(dice, passes, 2/3)), end="\t") 
+    for j in range(min(dice + 1, 10), 10):
+        print("0", end="\t")
+    print()
